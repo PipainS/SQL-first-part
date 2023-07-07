db5bdd58eda648886c70aed42eed732ada0af1a8
PipainS <70841962+PipainS@users.noreply.github.com>
2023-07-07 18:24:26 +0300

    Add files via upload

--- 07/Solution_file_SQL/e.sql
+++ 07/Solution_file_SQL/e.sql
@@ -1,8 +1,14 @@
-BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
-
-UPDATE accounts SET balance = balance - 100 WHERE id = 1;
-
-COMMIT;
-
-
-
+-- создаем таблицу для примера
+CREATE TABLE products (
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(100)
+);
+-- вставляем данные
+INSERT INTO products (name) VALUES ('Product 1'), ('Product 2');
+-- Открытие транзакций
+BEGIN;
+BEGIN;
+-- Обновление в первой транзакции
+UPDATE products SET name = 'Updated Product 1' WHERE id = 1;
+-- Попытка обновления во второй транзакции
+UPDATE products SET name = 'Updated Product 1' WHERE id = 1;
